39aec9f0436c8fca0329f4359c402ba6b1e8034d
taielfj <taielfernandezjara@gmail.com>
2022-05-13 02:01:22 -0300

    terminando las funciones del register

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,3 +1,9 @@
+const {getUsers, writeUsers} = require('../data/data');
+const {validationResult} = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+
+
 module.exports= {
     login: (req,res)=>{
         res.render("users/login",{
@@ -10,5 +16,40 @@ module.exports= {
             titulo: 'Registrarse'
         }
         )
+    },
+
+    newAcount: (req, res) => {
+        let errors = validationResult(req)
+        if(errors.isEmpty()){
+                let lastId = 0;
+
+            getUsers.forEach((e)=>{
+                if(e.id > lastId){
+                    lastId = e.id
+                }
+            })
+
+            let newUser = {
+                id: lastId + 1,
+                name: req.body.name,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                pass: bcrypt.hashSync(req.body.pass, 10), 
+                img: req.file ? req.file.filename : "defaultAvatar.png",
+                rol: "user"
+            }
+
+            getUsers.push(newUser)
+
+            writeUsers(getUsers)
+
+            res.redirect('/login')
+        }else{
+            res.render('users/register',{
+                titulo: 'Registrarse',
+                errors: errors.mapped(),
+                old: req.body
+            })
+        }
     }
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const router= express.Router();
+const validateRegister = require('../data/validations/validations');
+const userController= require("../controllers/userControllers");
+const fileUpload = require('../data/multer/multer');
 
-const userController= require("../controllers/userControllers")
+router.get("/login", userController.login);
+router.get("/register", userController.register);
 
-router.get("/login", userController.login)
-router.get("/register", userController.register)
+router.post('/register', fileUpload.single('avatar'), validateRegister, userController.newAcount);
 
 module.exports= router;
