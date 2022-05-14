67e93f0959dd799e50bf43fa7b719bba9acac110
taielfj <taielfernandezjara@gmail.com>
2022-05-13 21:28:30 -0300

    middleware admin

---
+++ src/middlewares/userAdminCheck.js
@@ -0,0 +1,9 @@
+const userAdminCheck = (req, res, next){
+    if(req.session.user.rol === "admin"){
+        next()
+    }else{
+        res.render("error")
+    }
+}
+
+module.exports = userAdminCheck;
