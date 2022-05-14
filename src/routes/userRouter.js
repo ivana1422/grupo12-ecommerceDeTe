73e165c0bfecb5c3905beed1be883a8d45ab7c3f
Adrian Gonzalez <adriramone90@gmail.com>
2022-05-13 21:20:39 -0300

    arreglando estilos por session

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -12,8 +12,9 @@ const validateRegister = require('../validations/registerValidator');
 const userOnline = require("../middlewares/userOnline")
 
 router.get("/login",userOnline, userController.login);
-router.post("login", loginValidator,userController.processLogin)
+router.post("/login", loginValidator,userController.processLogin)
 router.get("/register",userOnline, userController.register);
 router.post('/register', fileUpload.single('avatar'), validateRegister, userController.newAcount);
+router.get("/logout", userController.logout)
 
 module.exports= router;
