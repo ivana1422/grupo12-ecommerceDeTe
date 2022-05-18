04bec3ee82d2556d2baf6331ed668028db43ca20
AdriRamone90 <adriramone90@gmail.com>
2022-05-18 16:38:47 -0300

    solucionando errores

--- src/middlewares/uploadImgProducts.js
+++ src/middlewares/uploadImgProducts.js
@@ -1,9 +1,6 @@
-//Primero se requiere multer
-
 const multer = require("multer");
 const path = require("path");
 
-//Se declar la funcion storage que determina la ruta donde se guarda el archivo y el nombre!
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
        cb(null, path.join(__dirname, '../../public/img/products'));
@@ -12,9 +9,6 @@ const storage = multer.diskStorage({
        cb(null, `${Date.now()}_img_${path.extname(file.originalname)}`);  }
   })
 
-  //Se guarda toda la funcion en una variable
   const uploadImgProducts = multer({ storage });
 
-
-  //Se exporta
   module.exports = uploadImgProducts;
