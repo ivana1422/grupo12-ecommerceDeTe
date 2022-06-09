6fbf403835eaa0ff5f8ec0b076a4013e59f272ba
adriangonzalez-dev <adriangonzalez.dev90@gmail.com>
2022-06-09 14:24:42 -0300

    aplicando variable de entorno a db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,15 @@
- module.exports= {
+const process = require('process');
+require('dotenv').config();
+const PORT_DB = process.env.PORT_DB;
+
+module.exports= {
   "development": {
     "username": "root",
     "password": null,
     "database": "tea",
     "host": "127.0.0.1",
     "dialect": "mysql",
-    "port": "3306"
+    "port": `${PORT_DB}`
   },
   "test": {
     "username": "root",
