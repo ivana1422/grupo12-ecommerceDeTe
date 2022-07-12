d4cf7a9b9638733f3fc1ae709a5fc966242e1c90
adriangonzalez-dev <94702562+adriramone90@users.noreply.github.com>
2022-07-12 09:50:17 -0300

    pagina de productos con api - solucionando carrousel

--- public/js/products/productsFilter.js
+++ public/js/products/productsFilter.js
@@ -1,5 +1,132 @@
+let container = document.querySelector(".container-products")
+let selectCategories = document.getElementById("categories__filter")
+let selectPrice = document.getElementById('price__filter')
+let searchProduct = document.getElementById('search__product')
+
 URL_API = "http://localhost:5000/api/productos/"
 
-fetch(URL_API)
-.then(result=>result.json())
-.then(data=>console.log(data))
+fetch(`${URL_API}`)
+    .then(result=>result.json())
+    .then(data=>{
+        let productos = data.products
+        console.log(productos)
+        
+        productos.forEach(producto => {
+            container.innerHTML +=`<div class="item-products">
+            <a href="/productos/${producto.id}">
+            <img src="/img/products/${producto.images[0].src}" alt="${producto.name}" class="img-products">
+                                        <div>
+                                            <h4>${producto.name}</h4>
+                                            <p class="offer">$${(producto.price - (producto.price * (producto.discount / 100)))}</p>
+                                            <p class="actual">$${producto.price}</p>
+                                            </div>
+                                            </a>
+                                            </div>`
+                                        });
+                                        
+                                        let categorias = data.categories
+        categorias.forEach(categoria => {
+            selectCategories.innerHTML += `<option value="${categoria.id}" id="categoria${categoria.id}">${categoria.name}</option>`
+        })
+
+        selectCategories.addEventListener("click",(e)=>{
+            e.preventDefault()
+            let productoPorCategoria = []
+            
+            productos.forEach(producto=>{
+                producto.categories.forEach(category=>{
+                    if(category.id == +e.target.value){
+                        productoPorCategoria.push(producto)
+                    }
+                })
+            })
+            container.innerHTML = 'cargando...'
+            container.innerHTML = ''
+            productoPorCategoria.forEach(producto => {
+                container.innerHTML +=`<div class="item-products">
+                <a href="/productos/${producto.id}">
+                <img src="/img/products/${producto.images[0].src}" alt="${producto.name}" class="img-products">
+                                            <div>
+                                                <h4>${producto.name}</h4>
+                                                <p class="offer">$${(producto.price - (producto.price * (producto.discount / 100)))}</p>
+                                                <p class="actual">$${producto.price}</p>
+                                                </div>
+                                                </a>
+                                                </div>`
+                                            });
+        })
+
+        selectPrice.addEventListener("click",(e)=>{
+            e.preventDefault()
+            if(e.target.value == 'mayor'){
+                productos.sort(function (a, b) {
+                    if (a.price < b.price) {
+                      return 1;
+                    }
+                    if (a.price > b.price) {
+                      return -1;
+                    }
+                    // a must be equal to b
+                    return 0;
+                  });
+                  container.innerHTML = ''
+                  productos.forEach(producto => {
+                    container.innerHTML +=`<div class="item-products">
+                    <a href="/productos/${producto.id}">
+                    <img src="/img/products/${producto.images[0].src}" alt="${producto.name}" class="img-products">
+                                                <div>
+                                                    <h4>${producto.name}</h4>
+                                                    <p class="offer">$${(producto.price - (producto.price * (producto.discount / 100)))}</p>
+                                                    <p class="actual">$${producto.price}</p>
+                                                    </div>
+                                                    </a>
+                                                    </div>`
+                                                });
+            } else if(e.target.value == 'menor'){
+                productos.sort(function (a, b) {
+                    if (a.price > b.price) {
+                      return 1;
+                    }
+                    if (a.price < b.price) {
+                      return -1;
+                    }
+                    return 0;
+                  });
+                  container.innerHTML = ''
+                  productos.forEach(producto => {
+                    container.innerHTML +=`<div class="item-products">
+                    <a href="/productos/${producto.id}">
+                    <img src="/img/products/${producto.images[0].src}" alt="${producto.name}" class="img-products">
+                                                <div>
+                                                    <h4>${producto.name}</h4>
+                                                    <p class="offer">$${(producto.price - (producto.price * (producto.discount / 100)))}</p>
+                                                    <p class="actual">$${producto.price}</p>
+                                                    </div>
+                                                    </a>
+                                                    </div>`
+                                                });
+            }
+        })
+        searchProduct.addEventListener("keyup",(e)=>{
+            container.innerHTML=''
+            productos.forEach(producto=>{
+                if(producto.name.toLowerCase().includes(e.target.value.toLowerCase())){
+                    container.innerHTML += //html
+                        `<div class="item-products">
+                                <a href="/productos/${producto.id}">
+                                    <img src="/img/products/${producto.images[0].src}" alt="${producto.name}" class="img-products">
+                                    <div>
+                                        <h4>${producto.name}</h4>
+                                        <p class="offer">$${(producto.price - (producto.price * (producto.discount / 100)))}</p>
+                                        <p class="actual">$${producto.price}</p>
+                                    </div>
+                                </a>
+                            </div>`
+                } 
+            })
+        })
+
+
+        })
+
+
--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -21,7 +21,22 @@ module.exports = {
         .catch((error) => { res.send(error)})        
     },
     allProductsFilter:(req,res)=>{
-        db.categories.findAll({
+
+        let products = db.products.findAll({
+            include: [{ association: "images"}, { association: "ingredients"},{association:"categories"}]
+        })
+
+        let categories = db.categories.findAll()
+        
+        Promise.all([products,categories])
+            .then(result=>{
+                res.json({
+                    products:result[0],
+                    categories:result[1]
+                })
+            })
+
+        /*db.categories.findAll({
             includes:[{association:"products"}]
         })
         .then((categories)=>{
@@ -34,6 +49,6 @@ module.exports = {
                     categories
                 })
             })
-        })
+        })*/
     }
 }
