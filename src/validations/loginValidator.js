2dd8b95a37e0852580b1554e30f3cb1fae26e350
taielfj <taielfernandezjara@gmail.com>
2022-07-24 16:01:42 -0300

    errores

--- src/validations/loginValidator.js
+++ src/validations/loginValidator.js
@@ -1,43 +1,35 @@
 const {check, body}=require("express-validator");
 const bcrypt = require("bcryptjs");
 const db = require('../database/models');
-// const res = require("express/lib/response");
 
 let loginValidator = [
     check('email')
                 .notEmpty()
                 .isEmail().withMessage('Debe ingresar un email válido').bail(),
-
     body('pass').custom((value,{req})=>{
-        /*return db.users.findOne({
-            where: {
-                email: req.body.email
-            }
-        })        
-        .then((user) => {
-            if(user){
-                if(!bcrypt.compareSync(req.body.pass,user.pass)){
-                    return Promise.reject("Email o contraseña Incorrecta")
-                } 
-                return true
-            }
-              
-        })*/
         return db.users.findOne({
-            where:{
+            where: {
                 email:req.body.email
             }
         })
-        .then(user=>{
-            if(bcrypt.compareSync(req.body.pass,user.pass)){
-                return true
+        .then((user) => {
+            if(user !== null){
+                if(!bcrypt.compareSync(req.body.pass, user.pass)){
+                    return Promise.reject('credenciales invalidas')
+                }else{
+                    return true
+                }
+            }else{
+                return Promise.reject('credenciales invalidas')
             }
-    
-            return false;
-        })
-    }).withMessage("Email o contraseña Incorrecta"),
 
-    body('pass').notEmpty().withMessage('Ingrese su contraseña').bail()
+        })
+        .catch(error => {
+            return Promise.reject("email o contrase;a incorrecto")
+        })
+    }).withMessage("Email o contrase;a incorrecto"),
+    body('pass').notEmpty().withMessage('Ingrese su contraseña').bail(),
 ]
 
 module.exports= loginValidator;
+
