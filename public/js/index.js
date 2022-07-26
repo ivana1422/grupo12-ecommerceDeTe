72c640f71a6ec733c4e3102c162f76d6c8ee8ca3
adriangonzalez-dev <94702562+adriramone90@users.noreply.github.com>
2022-07-26 19:55:08 -0300

    Agregando slider a tarjetas de productos

--- public/js/index.js
+++ public/js/index.js
@@ -14,3 +14,20 @@ function shareProduct(id){
     })
 }
 
+const arrowSliderRight = document.querySelector('.arrowSliderRight')
+
+const arrowSliderLeft = document.querySelector('.arrowSliderLeft')
+
+const slider = document.querySelector('.slider')
+
+arrowSliderLeft.addEventListener('click',()=>{
+    slider.scrollLeft -= 300
+})
+
+arrowSliderRight.addEventListener('click',()=>{
+    slider.scrollLeft += 300
+})
+
+setInterval(()=>{
+    slider.scrollLeft += 300
+},4000)
