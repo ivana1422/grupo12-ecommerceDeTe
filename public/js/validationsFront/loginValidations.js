8adfe8b79288b0ecabc07cd538f6916dec5f3023
taielfj <taielfernandezjara@gmail.com>
2022-07-29 22:18:11 -0300

    resolviendo problemas en login y register

--- public/js/validationsFront/loginValidations.js
+++ public/js/validationsFront/loginValidations.js
@@ -1,59 +1,75 @@
-let expression={
-    name_surname: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
-    pass: /^.{8,12}$/, // 8 a 12 digitos.
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
-}
 
-const inputEmail = document.getElementById("input__login__email");
-const inputPass = document.getElementById("input__login__pass");
-const iconEmail = document.getElementById("icon__login__email");
-const iconPass = document.getElementById("icon__login__pass");
-const form = document.querySelector("form")
 
-let emailBoolean = false;
-let passBoolean = false;
+window.addEventListener('load', () =>{
 
-const validInput = (input,icon)=>{
-    input.classList.remove("input__invalid")
-    input.classList.add("input__valid")
-    icon.classList.remove("fa-circle-exclamation","icon__invalid")
-    icon.classList.add("fa-solid", "fa-circle-check","icon__valid")
-}
 
-const invalidInput = (input,icon)=>{
-    input.classList.remove("input__valid")
-    input.classList.add("input__invalid")
-    icon.classList.remove("fa-circle-check","icon__valid")
-    icon.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")
-}
+    let expression={
+        name_surname: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+        pass: /^.{8,12}$/, // 8 a 12 digitos.
+        email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    }
+    
+    const inputEmail = document.getElementById("input__login__email");
+    const inputPass = document.getElementById("input__login__pass");
+    const iconEmail = document.getElementById("icon__login__email");
+    const iconPass = document.getElementById("icon__login__pass");
+    const form = document.querySelector("form")
+    
+    let emailBoolean = false;
+    let passBoolean = false;
+    
+    const validInput = (input,icon)=>{
+        input.classList.remove("input__invalid")
+        input.classList.add("input__valid")
+        icon.classList.remove("fa-circle-exclamation","icon__invalid")
+        icon.classList.add("fa-solid", "fa-circle-check","icon__valid")
+    }
+    
+    const invalidInput = (input,icon)=>{
+        input.classList.remove("input__valid")
+        input.classList.add("input__invalid")
+        icon.classList.remove("fa-circle-check","icon__valid")
+        icon.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")
+    }
 
-//let errors = []
+    const validateEmail = (e) =>{
+        if(e.value == ''){
+            invalidInput(inputEmail,iconEmail)
+            emailBoolean = false
+            
+        } else {
+            if(expression.email.test(e)){
+                validInput(inputEmail,iconEmail)
+                emailBoolean = true
+               
+            }else{
+                invalidInput(inputEmail,iconEmail)
+                emailBoolean = false
+               
+            }
+        }
+    }
+
 
     inputEmail.addEventListener("keyup",(e)=>{
         if(e.target.value == ''){
             invalidInput(inputEmail,iconEmail)
             emailBoolean = false
+            
         } else {
             if(expression.email.test(e.target.value)){
                 validInput(inputEmail,iconEmail)
                 emailBoolean = true
-                /*inputEmail.classList.remove("input__invalid")
-                inputEmail.classList.add("input__valid")
-                iconEmail.classList.remove("fa-circle-exclamation","icon__invalid")
-                iconEmail.classList.add("fa-solid", "fa-circle-check","icon__valid")*/
+               
             }else{
                 invalidInput(inputEmail,iconEmail)
                 emailBoolean = false
-                /*inputEmail.classList.remove("input__valid")
-                inputEmail.classList.add("input__invalid")
-                iconEmail.classList.remove("fa-circle-check","icon__valid")
-                iconEmail.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")
-                let mailVacio = e.target.value
-                errors.push(mailVacio)*/
+               
             }
         }
         
     })
+
 
     inputPass.addEventListener("keyup",(e)=>{
         if(e.target.value == ''){
@@ -63,19 +79,9 @@ const invalidInput = (input,icon)=>{
             if(expression.pass.test(e.target.value)){
                 validInput(inputPass,iconPass)
                 passBoolean = true
-                /*inputPass.classList.remove("input__invalid")
-                inputPass.classList.add("input__valid")
-                iconPass.classList.remove("fa-circle-exclamation","icon__invalid")
-                iconPass.classList.add("fa-solid", "fa-circle-check","icon__valid")*/
             }else{
                 invalidInput(inputPass,iconPass)
                 passBoolean = false
-                /*inputPass.classList.remove("input__valid")
-                inputPass.classList.add("input__invalid")
-                iconPass.classList.remove("fa-circle-check","icon__valid")
-                iconPass.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")
-                let passVacio = e.target.value
-                errors.push(passVacio)*/
             }
         }
         
@@ -83,40 +89,24 @@ const invalidInput = (input,icon)=>{
 
     form.addEventListener('submit',(e)=>{
         e.preventDefault()
+        if (!emailBoolean){
+            validateEmail(inputEmail.value)
+        }
         if(emailBoolean && passBoolean){
             form.submit()
-        } else if (!emailBoolean || !passBoolean){
+        } else if (!emailBoolean){
             if(!inputEmail.classList.contains("input__valid")){
                 invalidInput(inputEmail,iconEmail)
-                /*inputEmail.classList.remove("input__valid")
-                inputEmail.classList.add("input__invalid")
-                iconEmail.classList.remove("fa-circle-check","icon__valid")
-                iconEmail.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")*/
             }
+            alert("Debe Completar 652626")
+        }else if (!passBoolean){
             if(!inputPass.classList.contains("input__valid")){
                 invalidInput(inputPass,iconPass)
-                /*inputPass.classList.remove("input__valid")
-                inputPass.classList.add("input__invalid")
-                iconPass.classList.remove("fa-circle-check","icon__valid")
-                iconPass.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")*/
             }
             alert("Debe Completar los campos")
         }
     })
-    /*form.addEventListener("submit",(e)=>{
-        e.preventDefault()
-        if(errors.length == 0){
+})
 
-            alert("Debe Completar los campos")
-            inputPass.classList.remove("input__valid")
-            inputPass.classList.add("input__invalid")
-            iconPass.classList.remove("fa-circle-check","icon__valid")
-            iconPass.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")
-            inputEmail.classList.remove("input__valid")
-            inputEmail.classList.add("input__invalid")
-            iconEmail.classList.remove("fa-circle-check","icon__valid")
-            iconEmail.classList.add("fa-solid","fa-circle-exclamation","icon__invalid")
-        } else if(inputEmail.classList.contains("input__valid") && inputPass.classList.contains("input__valid")){
-            form.submit()
-        }
-    })*/
+
+    
